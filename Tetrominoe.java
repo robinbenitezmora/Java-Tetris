e8a7b400d5255cba91d07cc2904a88490949c8ff
robinbenitezmora <robinbenitez1990@gmail.com>
2024-01-30 19:07:20 -0500

    Add code Tetrominoe.java class

--- Tetrominoe.java
+++ Tetrominoe.java
@@ -5,13 +5,16 @@ public class Tetrominoe {
  public static Tetrominoe NoShape;
 
  public int getValue() {
-  // TODO Auto-generated method stub
-  throw new UnsupportedOperationException("Unimplemented method 'getValue'");
+  if (this == SQUARE_SHAPE) {
+   return 0;
+  } else if (this == NoShape) {
+   return 0;
+  } else {
+   return 0;
+  }
  }
 
  public static Tetrominoe[] values() {
-  // TODO Auto-generated method stub
-  throw new UnsupportedOperationException("Unimplemented method 'values'");
+  return new Tetrominoe[] { SQUARE_SHAPE, NoShape };
  }
-
 }
